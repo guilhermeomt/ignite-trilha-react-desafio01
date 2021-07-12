391a8ad495e81aab07db52da79a1d8fe13a67c67
Guilherme <guilhermet213@gmail.com>
2021-07-12 17:36:31 -0300

    feat: implement create task functionality

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,16 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (!newTaskTitle) return;
+
+    const newTask: Task = {
+      id: Math.floor(Math.random() * 5000),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
